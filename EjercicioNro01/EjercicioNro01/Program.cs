80488fa5417272bf21bcd816da9c3c52a3461ffe
ezequielfreire007 <ezequiel.a.freire@gmail.com>
2016-03-16 14:36:04 -0300

    Segundo comit, se realiza maximo, minimo y promedio.

--- EjercicioNro01/EjercicioNro01/Program.cs
+++ EjercicioNro01/EjercicioNro01/Program.cs
@@ -11,7 +11,35 @@ namespace EjercicioNro01
         static void Main(string[] args)
         {
             Console.Title = "Ejercicio Nro 01";
+            String dato = null;
+            int numero = 0;
+            int[] num = new int [5];
+            float promedio = 0;
+            int sum = 0;
+            
+            for (int i = 0; i < num.Length; i++)
+            {
+                Console.WriteLine("Ingrese un numero: ");
+                dato = Console.ReadLine();
+                while (int.TryParse(dato, out numero) == false)
+                {
+                    Console.WriteLine("Error reingrese un numero.");
+                    dato = Console.ReadLine();
 
+                }
+                num[i] = int.Parse(dato);
+            }
+
+            for (int i = 0; i < num.Length; i++)
+            {
+                sum = sum + num[i];
+            }
+            promedio = sum / num.Length;
+
+            Console.WriteLine("Valor Maximo: ", num.Max());
+            Console.WriteLine("Valor Minimo: ", num.Min());
+            Console.WriteLine("Valor Promedio: ", promedio);
+            
         }
     }
 }
