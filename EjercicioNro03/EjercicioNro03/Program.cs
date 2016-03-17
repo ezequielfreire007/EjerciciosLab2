493c0a479d54d9e715d913dfcac066309ccecc38
ezequielfreire007 <ezequiel.a.freire@gmail.com>
2016-03-17 14:59:09 -0300

    Se realiza el ejercicio Nro 03

---
+++ EjercicioNro03/EjercicioNro03/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EjercicioNro03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Ejercicio Nro 10";
+
+            String dato = null;
+            int numero = 0;
+            int [] primo;
+            
+
+            Console.WriteLine("Ingrese un numero: ");
+            dato = Console.ReadLine();
+
+            while(int.TryParse(dato,out numero) == false)
+            {
+                Console.WriteLine("Debe ingresar un numero. Intentelo nuevamente");
+                dato = Console.ReadLine();
+            }
+
+            for (int j = numero; numero <= 2; j--)
+            {
+                int numeroPrimo = numero;
+                int cont = 0;
+                int cont2 = 0;
+                while (cont == 0)
+                {
+                    for (int j = numeroPrimo - 1; numeroPrimo <= 2; j--)
+                    {
+                        if (numero % numeroPrimo == 0)
+                        {
+                            cont++;
+                        } 
+                    }
+                    
+                    primo[cont2] = numero;
+                    cont2++;
+                }
+            }
+
+            Console.WriteLine("Numeros primos");
+            for(int i = 0; i < primo.Length; i++)
+            {
+
+            }
+
+        }
+    }
+}
