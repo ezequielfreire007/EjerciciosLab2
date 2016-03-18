2989e670ec0f867c2cd2dd695ca0283393cdab42
ezequielfreire007 <ezequiel.a.freire@gmail.com>
2016-03-17 23:00:27 -0300

    Creacion y primer commit de ejercicio Nro 04

---
+++ EjercicioNro04/EjercicioNro04/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EjercicioNro04
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Ejercicio Nro 04";
+
+            String dato = null;
+            int numero = 0;
+            bool bandera = true;
+            int [] numDivisor;
+            int cont = 0;
+
+            Console.WriteLine("Ingrese un numero: ");
+            dato = Console.ReadLine();
+
+            while(int.TryParse(dato, out numero) == false)
+            {
+                Console.WriteLine();
+            }
+
+            do
+            {
+
+
+            }while(bandera == true);
+
+        }
+    }
+}
