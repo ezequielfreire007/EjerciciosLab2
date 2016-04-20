3fe88bba7d3c5cbf8ac01e34194321fa6008c4d2
ezequielFreire007 <ezequiel.a.freire@gmail.com>
2016-04-19 22:16:45 -0300

    Se agrega ejercicio n°24 y 25

---
+++ EjercicioNro24/EjercicioNro24/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EjercicioNro24
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Ejercicio Nro 24";
+
+            int [] A = new int[10] {1,2,3,5,4,8,9,7,8,5};
+            int [] B = new int[10] {5,2,3,4,8,7,8,2,7,4};
+            int [] C = new int[10];
+
+            for (int i = 0; i < A.Count(); i++ )
+            {
+                if (A[i] == B[i])
+                {
+                    C[i] = A[i];
+                }
+                else if (A[i] < B[i])
+                {
+                    C[i] = A[i];
+                }
+                else
+                {
+                    C[i] = B[i];
+                }
+            }
+
+            Console.WriteLine("Vector A");
+            foreach (int item in A)
+            {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine("Vector B");
+            Console.WriteLine("-----------------");
+            foreach (int item in B)
+            {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine("Vector C");
+            Console.WriteLine("-----------------");
+            foreach (int item in C)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.WriteLine("Ordenar decendente");
+            Array.Sort(C);
+            Console.WriteLine("Vector C");
+            Console.WriteLine("-----------------");
+            foreach (int item in C)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.WriteLine("Ordenar acendente");
+            Array.Reverse(C);
+            Console.WriteLine("Vector C");
+            Console.WriteLine("-----------------");
+            foreach (int item in C)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ EjercicioNro25/EjercicioNro25/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EjercicioNro25
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int cont = 0;
+            int cont1 = 0;
+            int [] numeros = new int[20] { 1, 5, 2, 3, 4, 7, 6, 9, 10, 8,
+                                          -8, -9, -5, -6, -1, -2, -3, -8, -6, -7 };
+            
+            int [] positivos = new int[10];
+            int[] negativos = new int[10];
+
+            //Mostrar vector inicial
+            Console.WriteLine("-------------------------------------");
+            Console.WriteLine("\t Vector inicial");
+            Console.WriteLine("-------------------------------------");
+            foreach (int item in numeros)
+            {
+                Console.WriteLine("\t{0}",item);
+            }
+            Console.WriteLine("-------------------------------------");
+
+            //Numeros positivos
+            foreach (int numero in numeros)
+            {
+                if (numero > 0)
+                {
+                    positivos[cont] = numero;
+                    cont++;
+                }
+                else
+                {
+                    negativos[cont1] = numero;
+                    cont1++;
+                }
+            }
+
+            //Mostrar positivos en forma decreciente
+            Array.Sort(positivos);
+            Console.WriteLine("Numeros positivos en forma decreciente");
+            Console.WriteLine("-------------------------------------");
+            foreach (int item in positivos)
+            {
+                Console.WriteLine("{0}", item);
+            }
+
+            Console.WriteLine("-------------------------------------");
+            //Mostrar negativos en forma creciente
+            Array.Sort(negativos);
+            Array.Reverse(negativos);
+            Console.WriteLine("Numeros negativos en forema creciente");
+            Console.WriteLine("-------------------------------------");
+            foreach (int item in negativos)
+            {
+                Console.WriteLine("{0}",item);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
